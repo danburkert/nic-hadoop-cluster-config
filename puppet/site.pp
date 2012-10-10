52472f7132e04576783331fc6f83c8eb6b241b97
Dan Burkert <danburkert@gmail.com>
2012-10-10 09:57:45 -0400

    first shot at bonding network interfaces

--- puppet/site.pp
+++ puppet/site.pp
@@ -16,4 +16,41 @@ node /^smmc\d+\.nearinfinity\.com$/ {
   notify { 'log message':
     message => "Puppet agent run on machine " + $hostname,
   }
+
+  user { 'localadmin':
+      ensure => present,
+      password => 'localadmin',
+      gid => 'localadmin',
+  }
+
+  group { 'localadmin':
+      ensure => present,
+  }
+
+  Group['localadmin'] -> User['localadmin']
+
+  user { 'hadoop':
+      ensure => present,
+      gid => 'hadoop',
+  }
+
+  group { 'hadoop':
+      ensure => present,
+  }
+
+  Group['hadoop'] -> User['hadoop']
+
+
+  network::bond::dynamic { "bond0":
+    ensure => "up",
+  }
+  network::bond::slave { "eth0":
+    macaddress => $macaddress_eth0,
+    master => "bond0",
+  }
+  network::bond::slave { "eth1":
+    macaddress => $macaddress_eth1,
+    master => "bond0",
+  }
+
 }
