29762b26c8e2dd7e521abbb0c9251621e0701dd7
Local Administrator <localadmin@nic-hadoop-puppet.(none)>
2012-10-10 09:35:07 -0400

    add site.pp to scripts

---
+++ puppet/site.pp
@@ -0,0 +1,13 @@
+node 'nic-hadoop-razor.nearinfinity.com' {
+  class { 'sudo':
+    config_file_replace => false,
+  }
+
+  class { 'razor':
+    address => '192.168.203.12',
+  }
+
+  class { 'tftp':
+    inetd => false,
+  }
+}
