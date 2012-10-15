a8667776ed59f94ef719a8d5114a806e14e2f034
Local Administrator <localadmin@nic-hadoop-razor.(none)>
2012-10-15 18:15:08 -0400

    update razor rebuilding scripts for CentOS

--- razor/setup-razor.sh
+++ razor/setup-razor.sh
@@ -1,36 +1,24 @@
 ### Add Images
-ubuntu_type='os'
-ubuntu_path='/opt/image/ubuntu-12.04.1-server-amd64.iso'
-ubuntu_name='ubuntu-precise'
-ubuntu_version='precise'
+image_type='os'
+image_path='/opt/image/CentOS-6.3-x86_64-minimal.iso'
+image_name='centos_6'
+image_version='6'
 
-ubuntu_uuid=`razor image add -t ${ubuntu_type} -p ${ubuntu_path} \
-  -n ${ubuntu_name} -v ${ubuntu_version} \
+image_uuid=`razor image add -t ${image_type} -p ${image_path} \
+  -n ${image_name} -v ${image_version} \
   | grep UUID | awk '{ print $3 }'`
-echo "Ubuntu uuid: ${ubuntu_uuid}"
+echo "Image uuid: ${image_uuid}"
 
 mk_type='mk'
-mk_path='/opt/image/rz_mk_prod-image.0.9.0.4.iso'
+mk_path='/opt/image/rz_mk_prod-image.0.9.1.6.iso'
 mk_uuid=`razor image add -t ${mk_type} -p ${mk_path} \
   | grep UUID | awk '{ print $3 }'`
 echo "mk uuid: ${mk_uuid}"
 
-### Create Broker
-broker_plugin='puppet'
-broker_name='puppet'
-broker_description='puppet with version'
-broker_server='nic-hadoop-puppet.nearinfinity.com'
-broker_version='2.7.19'
-
-broker_uuid=`razor broker add -p ${broker_plugin} -n ${broker_name} \
-  -d ${broker_description} -s ${broker_server} -v ${broker_version} \
-  | grep UUID | awk '{ print $3 }'`
-echo "broker uuid: ${broker_uuid}"
-
 ### Create Model
-model_template='nic_hadoop_ubuntu_precise'
+model_template='nic_hadoop_centos_6'
 model_label='nic-hadoop-model'
-model_image=${ubuntu_uuid}
+model_image=${image_uuid}
 
 model_uuid=`razor model add -t ${model_template} -l ${model_label} \
   -i ${model_image} \
@@ -41,11 +29,13 @@ echo "model uuid: ${model_uuid}"
 policy_template='linux_deploy'
 policy_label='nic-hadoop-policy'
 policy_model=${model_uuid}
-policy_broker=${broker_uuid}
 policy_tags='Supermicro'
 policy_enabled='true'
 
 policy_uuid=`razor policy add -p ${policy_template} -l ${policy_label} \
-  -m ${policy_model} -b ${policy_broker} -t ${policy_tags} -e ${policy_enabled}\
+  -m ${policy_model} -t ${policy_tags} -e ${policy_enabled}\
   | grep UUID | awk '{ print $3 }'`
 echo "policy uuid: ${policy_uuid}"
+
+### Configure iPXE settings
+sudo razor config ipxe > /var/lib/tftpboot/razor.ipxe
