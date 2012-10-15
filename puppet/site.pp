d2601951cdb78a476e722997bf514b1cd8514d24
Local Administrator <localadmin@nic-hadoop-puppet.(none)>
2012-10-15 18:17:13 -0400

    add default site.pp with instructions for razor and cloud nodes

--- puppet/site.pp
+++ puppet/site.pp
@@ -4,16 +4,25 @@ node 'nic-hadoop-razor.nearinfinity.com' {
   }
 
   class { 'razor':
-    address => '192.168.203.12',
+    username => 'razor',
   }
 
   class { 'tftp':
     inetd => false,
   }
+  exec { 'razor-mk_uri':
+    command => "/bin/sed -i'' -e 's|mk_uri: http://.*:8026|mk_uri: http://${razor::address}:8026|' /opt/razor/conf/razor_server.conf",
+    unless => "/bin/grep -q 'mk_uri: http://${razor::address}:8026' /opt/razor/conf/razor_server.conf",
+    require => Service['razor'],
+    notify => Exec['restart-razor'],
+  }
+  exec { 'restart-razor':
+    command => "/opt/razor/bin/razor_daemon.rb restart",
+  }
 }
 
-node /^smmc\d+\.nearinfinity\.com$/ {
+node /^nic-hadoop-smmc\d+\.hadoop\.nearinfinity\.com$/ {
   notify { 'log message':
-    message => "Puppet agent run on machine " + $hostname,
+    message => "Puppet agent run on machine ${hostname}",
   }
 }
