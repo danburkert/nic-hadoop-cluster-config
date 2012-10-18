9ea9d525501b99562150fc6bc797420195aa0aa1
Dan Burkert <danburkert@gmail.com>
2012-10-18 17:04:45 -0400

    move hadoop config files for alternatives support

--- hadoop-0.20/conf.nic-hadoop/hadoop-env.sh
+++ hadoop-0.20/conf.nic-hadoop/hadoop-env.sh
@@ -6,8 +6,8 @@
 # remote nodes.
 
 # The java implementation to use.  Required.
+# export JAVA_HOME=/usr/lib/j2sdk1.6-sun
 export JAVA_HOME=/usr/java/default
-
 
 # Extra Java CLASSPATH elements.  Optional.
 # export HADOOP_CLASSPATH="<extra_entries>:$HADOOP_CLASSPATH"
